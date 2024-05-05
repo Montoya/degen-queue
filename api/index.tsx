180f736de1a0f5b83a91965053415caf56880ef0
Christian Montoya <christian.montoya@consensys.net>
2024-05-04 22:37:03 -0700

    Better UI

--- api/index.tsx
+++ api/index.tsx
@@ -5,8 +5,16 @@ import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 import { NeynarAPIClient } from '@neynar/nodejs-sdk'
+
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  vars,
+} from './ui.js'
 
 const prisma = new PrismaClient();
 
@@ -20,7 +28,8 @@ export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
   // Supply a Hub to enable frame verification.
-  hub: neynar({ apiKey: `${process.env.NEYNAR_API_KEY}` })
+  hub: neynar({ apiKey: `${process.env.NEYNAR_API_KEY}` }),
+  ui: { vars }
 })
 
 app.get('/db-read', async (c) => { 
@@ -59,21 +68,34 @@ app.get('/queue/:fid', async (c) => {
       }
     })
 
-    // fetch all the user data we need 
-    const authorIds:Array<number> = []
-    // fetch all the cast data we need 
-    const castHashes:Array<string> = []
-    rows.map((row:any) => {
-      if(!authorIds.includes(row['castAuthor'])) { 
-        authorIds.push(row['castAuthor'])
+    let authors:Array<any> = []; 
+    let casts:Array<any> = []; 
+    let tipAmount = remaining; 
+    let finalTipAmount = remaining; 
+    if(rows.length > 0) { 
+      // fetch all the user data we need 
+      const authorIds:Array<number> = []
+      // fetch all the cast data we need 
+      const castHashes:Array<string> = []
+      rows.map((row:any) => {
+        if(!authorIds.includes(row['castAuthor'])) { 
+          authorIds.push(row['castAuthor'])
+        }
+        castHashes.push(row['castHash'])
+      })
+
+      const authorResponse = await neynarClient.fetchBulkUsers(authorIds)
+      authors = authorResponse.users
+
+      const castResponse = await neynarClient.fetchBulkCasts(castHashes)
+      casts = castResponse.result?.casts
+
+      if(casts.length > 0) { 
+        tipAmount = Math.ceil(remaining / casts.length)
+        let excess = (tipAmount * casts.length) - remaining; 
+        finalTipAmount = tipAmount - excess; 
       }
-      castHashes.push(row['castHash'])
-    })
-
-    const authorResponse = await neynarClient.fetchBulkUsers(authorIds);
-    const authors = authorResponse.users; 
-
-    const casts = await neynarClient.fetchBulkCasts(castHashes); 
+    }
 
     return c.html(
       <html>
@@ -83,29 +105,53 @@ app.get('/queue/:fid', async (c) => {
           <link rel="stylesheet" href="/styles.css"/>
         </head>
         <body>
-          <h1>Your $DEGEN Queue</h1>
-          <p>Your $DEGEN allowance for today: {allowance}</p>
-          <p>Your remaining balance for today: {remaining}</p>
-          <p>{rows.length} casts in your queue:</p>
-          {rows.length > 0 &&
-            <table>
-              {rows.map((row:any, index:number) => (
-                <tr key={index}>
-                  <td>{row['castAuthor']}</td>
-                  <td>{row['castHash']}</td>
+          <div id="page">
+            <h1>Your $DEGEN Queue</h1>
+            <p>Your $DEGEN allowance: {allowance}</p>
+            <p>Your remaining balance: {remaining}</p>
+            <p>{casts?.length} casts in your queue:</p>
+            {casts.length > 0 &&
+              <table>
+                <tr>
+                  <th>Author</th>
+                  <th>Cast URL</th>
+                  <th>Amount to tip</th>
                 </tr>
-              ))}
-            </table>
-          }
-          {rows.length < 1 && 
-            <p><em>Add some casts to get started!</em></p>
-          }
-          <p>{JSON.stringify(authors)}</p>
-          <p>{JSON.stringify(casts)}</p>
+                {casts?.map((row:any, index:number) => (
+                  <tr key={index}>
+                    <td>
+                      <img src={row["author"]["pfp_url"]} width="24" height="24"/> 
+                      {row["author"]["display_name"]}
+                    </td>
+                    <td>
+                      <a href={
+                        "https://warpcast.com/"+
+                        row["author"]["username"]+
+                        "/"+
+                        row["hash"].slice(0,10)
+                      } target="_blank">URL</a>
+                    </td>
+                    <td>
+                      {index==(casts.length-1) ? finalTipAmount : tipAmount}
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            }
+            {casts.length < 1 && 
+              <p><em>Add some casts to get started!</em></p>
+            }
+          </div>
         </body>
       </html>
     )
-    return c.text(`al: ${allowance}, re: ${remaining}`)
+    /*
+
+            <h2>Authors</h2>
+            <textarea>{JSON.stringify(authors)}</textarea>
+            <h2>Casts</h2>
+            <textarea>{JSON.stringify(casts)}</textarea>
+    */
   }
   else { 
     return c.text("Not found or no allowance")
@@ -115,9 +161,20 @@ app.get('/queue/:fid', async (c) => {
 app.frame('/', (c) => {
   return c.res({
     image: (
-      <div style={{ color: 'purple', display: 'flex', fontSize: 32, padding: 16 }}>
-        Add "Degen Queue" Action
-      </div>
+      <Box 
+        grow 
+        backgroundColor={'background200'}
+        padding='32' 
+        alignVertical='top'
+      >
+        <VStack gap='24'>
+          <Heading>Degen Queue Action</Heading>
+          <Text>Ever struggle to keep track of all the great casts you want to tip?</Text>
+          <Text>Degen Queue makes it easy!</Text>
+          <Text>With the Degen Queue action, you can add your favorite casts to a queue and then allocate your $DEGEN tip balance fairly.</Text>
+          <Text>Just add the cast action below, then add casts to your queue to get started.</Text>
+        </VStack>
+      </Box>
     ),
     intents: [
       <Button.AddCastAction action="/add-to-degen-queue">
@@ -170,9 +227,18 @@ app.frame('/degen-queue-frame-response', async (c) => {
     const redirectLocation = `https://degen-queue.vercel.app/api/queue/${viewer}`
     return c.res({
       image: (
-        <div style={{ color: 'purple', display: 'flex', fontSize: 60 }}>
-          You have {castCount} casts in your Degen Queue
-        </div>
+        <Box 
+        grow 
+        backgroundColor={'background200'}
+        padding='32' 
+        alignVertical='top'
+      >
+        <VStack gap='32'>
+          <Heading>Cast Added!</Heading>
+          <Text>You have {castCount} casts in your Degen Queue.</Text>
+          <Text>Click the button to see your queue and start tipping!</Text>
+        </VStack>
+      </Box>
       ),
       intents: [
         <Button.Link href={redirectLocation}>View My Queue</Button.Link>
@@ -182,9 +248,17 @@ app.frame('/degen-queue-frame-response', async (c) => {
   catch (error) { 
     return c.res({
       image: (
-        <div style={{ color: 'purple', display: 'flex', fontSize: 60 }}>
-          Error: `${error}`
-        </div>
+        <Box 
+        grow 
+        backgroundColor={'background200'}
+        padding='32' 
+        alignVertical='top'
+      >
+        <VStack gap='32'>
+          <Heading>Error</Heading>
+          <Text>{error as string}</Text>
+        </VStack>
+      </Box>
       )
     })
   }
